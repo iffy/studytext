cdf991d921aa92535467da47ac810c54526c8b81
Matt Haggard <haggardii@gmail.com>
2016-01-31 22:37:29 -0700

    Sort of how I want

--- db.py
+++ db.py
@@ -1,8 +1,13 @@
 import yaml
 import os
+import re
+from uuid import uuid4
+from datetime import datetime
 
 class NoteDatabase(object):
-    
+ 
+    notes_path = '_notes'
+
     def __init__(self, root):
         self.root = os.path.abspath(root)
 
@@ -19,6 +24,8 @@ class NoteDatabase(object):
         List the documents and folders at this path.
         """
         p = self.safePath(path)
+        if not os.path.isdir(p):
+            return None
         return [self.info(os.path.join(p, x)) \
             for x in os.listdir(p) if not(x.startswith('_'))]
 
@@ -33,13 +40,75 @@ class NoteDatabase(object):
             'file': os.path.isfile(p),
         }
         info['parent'] = os.path.join(info['path'], '..')
+        info['name'] = os.path.basename(p)
         if info['dir']:
+            # directory
             meta_filename = os.path.join(p, '_meta.yml')
             if os.path.exists(meta_filename):
                 metadata = yaml.safe_load(open(meta_filename, 'rb'))
                 if metadata:
                     info.update(metadata)
+        else:
+            # file
+            if p.endswith('.md'):
+                metadata = yaml.safe_load_all(open(p, 'rb')).next()
+                if metadata and isinstance(metadata, dict):
+                    info.update(metadata)
         return info
+
+
+    def getContent(self, path):
+        """
+        Get the content of a file.
+        """
+        p = self.safePath(path)
+        content = open(p, 'rb').read()
+        if content.startswith('---'):
+            return content[content.index('---', 1)+3:].strip()
+        return content
+
+    def _noteDir(self, path, collection):
+        p = self.safePath(path)
+        relpart = os.path.relpath(p, self.root)
+        return os.path.join(self.root, '_notes', collection, relpart)
+
+    def addNote(self, path, start, end, text, collection='default'):
+        """
+        Add a note for the text in a file.
+        """
+        note_file = os.path.join(self._noteDir(path, collection), 'notes.yml')
+        dirname = os.path.dirname(note_file)
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+        current = []
+        if os.path.exists(note_file):
+            current = yaml.safe_load(open(note_file, 'rb').read())
+        current.append({
+            'id': str(uuid4()),
+            'date': datetime.now().isoformat(),
+            'start': start,
+            'end': end,
+            'text': text,
+        })
+        with open(note_file, 'wb') as fh:
+            fh.write(yaml.safe_dump(current, default_flow_style=False))
+        return current[-1]
+
+    def listNotes(self, path, collection='default'):
+        """
+        List notes for a file.
+        """
+        note_file = os.path.join(self._noteDir(path, collection), 'notes.yml')
+        if os.path.exists(note_file):
+            return yaml.safe_load(open(note_file, 'rb').read())
+        else:
+            return []
+
+    def updateNote(self, path, note_id, text, collection='default'):
+        """
+        Update an existing note.
+        """
+        raise NotImplementedError("Not implemented yet")
 
 
 
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template, g, redirect, url_for
+from flask import request
+
+import os
 
 from db import NoteDatabase
 
@@ -7,20 +10,49 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('notes'))
+    return redirect(url_for('text'), code=302)
 
-@app.route('/notes/', defaults={'path': ''})
-@app.route('/notes/<path:path>')
-def notes(path):
+@app.route('/text/', defaults={'path': ''})
+@app.route('/text/<path:path>')
+def text(path):
     print 'path', path
-    documents = app.config.db.list(path)
     info = app.config.db.info(path)
+    documents = []
+    content = ''
+    notes = []
+    if info['dir']:
+        documents = app.config.db.list(path)
+    else:
+        documents = app.config.db.list(os.path.join(path, '..'))
+        content = app.config.db.getContent(path)
+        notes = app.config.db.listNotes(path)
 
-    return render_template('notes.html',
+    print 'notes', notes
+
+    return render_template('text.html',
+        content=content,
+        notes=notes,
         path=path,
         info=info,
         title=info.get('title', '?'),
         documents=documents)
+
+
+@app.route('/notes', methods=['POST'])
+def notes():
+    path = request.values['path']
+    if 'note_id' in request.values:
+        # update
+        note_id = request.values['note_id']
+        app.config.db.updateNote()
+    else:
+        # add
+        start = request.values['start']
+        end = request.values['end']
+        text = request.values['text']
+        if text:
+            app.config.db.addNote(path, start, end, text)
+    return redirect(url_for('text', path=path), code=302)
 
 
 if __name__ == '__main__':
